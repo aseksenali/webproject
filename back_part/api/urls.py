569c4d6f914d43432ab7bfc5d38d61859b5f2999
aseksenali <asetseksenali@gmail.com>
2020-04-28 23:21:56 +0600

    Sorry for this mistake

--- back_part/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from api import views
-
-urlpatterns = [
-    path('api/books', views.BookListAPIView.as_view()),
-    path('api/genres', views.GenreListAPIView.as_view()),
-    path('api/authors', views.AuthorListAPIView.as_view()),
-]
